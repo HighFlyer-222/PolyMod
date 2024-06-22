7b499b51a2a103e08890a66cf2382f771fd00b67
HighFlyer222 <hflyer222@protonmail.com>
2024-06-22 22:18:33 +0200

    Comment out sprite-related code

--- src/ModLoader.cs
+++ src/ModLoader.cs
@@ -15,7 +15,7 @@ namespace PolyMod
 	{
 		private static int _autoidx = Plugin.AUTOIDX_STARTS_FROM;
 		private static List<JObject> _patches = new();
-		private static Dictionary<string, byte[]> _sprites = new();
+		// private static Dictionary<string, byte[]> _sprites = new();
 		private static Dictionary<string, AudioClip> _audios = new();
 
 		[HarmonyPrefix]
@@ -32,61 +32,61 @@ namespace PolyMod
 			__result = (int)type >= Plugin.AUTOIDX_STARTS_FROM || __result;
 		}
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetTileSpriteAddress), new Type[] { typeof(Polytopia.Data.TerrainData.Type), typeof(string) })]
-		private static void SpriteData_GetTileSpriteAddress(ref SpriteAddress __result, Polytopia.Data.TerrainData.Type terrain, string skinId)
-		{
-			__result = GetSprite(__result, EnumCache<Polytopia.Data.TerrainData.Type>.GetName(terrain), skinId);
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetTileSpriteAddress), new Type[] { typeof(Polytopia.Data.TerrainData.Type), typeof(string) })]
+		// private static void SpriteData_GetTileSpriteAddress(ref SpriteAddress __result, Polytopia.Data.TerrainData.Type terrain, string skinId)
+		// {
+			// __result = GetSprite(__result, EnumCache<Polytopia.Data.TerrainData.Type>.GetName(terrain), skinId);
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetResourceSpriteAddress), new Type[] { typeof(ResourceData.Type), typeof(string) })]
-		private static void SpriteData_GetResourceSpriteAddress(ref SpriteAddress __result, ResourceData.Type type, string skinOrTribeAsString)
-		{
-			__result = GetSprite(__result, EnumCache<ResourceData.Type>.GetName(type), skinOrTribeAsString);
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetResourceSpriteAddress), new Type[] { typeof(ResourceData.Type), typeof(string) })]
+		// private static void SpriteData_GetResourceSpriteAddress(ref SpriteAddress __result, ResourceData.Type type, string skinOrTribeAsString)
+		// {
+			// __result = GetSprite(__result, EnumCache<ResourceData.Type>.GetName(type), skinOrTribeAsString);
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetBuildingSpriteAddress), new Type[] { typeof(ImprovementData.Type), typeof(string) })]
-		private static void SpriteData_GetBuildingSpriteAddress(ref SpriteAddress __result, ImprovementData.Type type, string climateOrSkinAsString)
-		{
-			__result = GetSprite(__result, EnumCache<ImprovementData.Type>.GetName(type), climateOrSkinAsString);
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetBuildingSpriteAddress), new Type[] { typeof(ImprovementData.Type), typeof(string) })]
+		// private static void SpriteData_GetBuildingSpriteAddress(ref SpriteAddress __result, ImprovementData.Type type, string climateOrSkinAsString)
+		// {
+			// __result = GetSprite(__result, EnumCache<ImprovementData.Type>.GetName(type), climateOrSkinAsString);
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetUnitIconAddress))]
-		private static void SpriteData_GetUnitIconAddress(ref SpriteAddress __result, UnitData.Type type)
-		{
-			__result = GetSprite(__result, "icon", EnumCache<UnitData.Type>.GetName(type));
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetUnitIconAddress))]
+		// private static void SpriteData_GetUnitIconAddress(ref SpriteAddress __result, UnitData.Type type)
+		// {
+			// __result = GetSprite(__result, "icon", EnumCache<UnitData.Type>.GetName(type));
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SkinType) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SkinType skin)
-		{
-			__result = GetSprite(__result, "head", EnumCache<SkinType>.GetName(skin));
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SkinType) })]
+		// private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SkinType skin)
+		// {
+			// __result = GetSprite(__result, "head", EnumCache<SkinType>.GetName(skin));
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(TribeData.Type) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, TribeData.Type type)
-		{
-			__result = GetSprite(__result, "head", EnumCache<TribeData.Type>.GetName(type));
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(TribeData.Type) })]
+		// private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, TribeData.Type type)
+		// {
+			// __result = GetSprite(__result, "head", EnumCache<TribeData.Type>.GetName(type));
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SpriteData.SpecialFaceIcon) })]
-		private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SpriteData.SpecialFaceIcon specialId)
-		{
-			__result = GetSprite(__result, "head", specialId.ToString());
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHeadSpriteAddress), new Type[] { typeof(SpriteData.SpecialFaceIcon) })]
+		// private static void SpriteData_GetHeadSpriteAddress(ref SpriteAddress __result, SpriteData.SpecialFaceIcon specialId)
+		// {
+			// __result = GetSprite(__result, "head", specialId.ToString());
+		// }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAvatarPartSpriteAddress))]
-		private static void SpriteData_GetAvatarPartSpriteAddress(ref SpriteAddress __result, string sprite)
-		{
-			__result = GetSprite(__result, sprite, "");
-		}
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAvatarPartSpriteAddress))]
+		// private static void SpriteData_GetAvatarPartSpriteAddress(ref SpriteAddress __result, string sprite)
+		// {
+			// __result = GetSprite(__result, sprite, "");
+		// }
 
 		// [HarmonyPostfix]
 		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetHouseAddresses), new Type[] { typeof(int), typeof(string), typeof(SkinType) })]
@@ -103,63 +103,63 @@ namespace PolyMod
 		// 	__result = sprites.ToArray();
 		// }
 
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAddress))]
-		public static bool SpriteData_GetAddress(ref SpriteAddress __result, PickerType pickerType, string skinID)
-		{
-			switch (pickerType)
-			{
-				case PickerType.Head:
-					__result = SpriteData.GetHeadSpriteAddress(skinID);
-					break;
-				case PickerType.Unit:
-					__result = new SpriteAddress("Units", skinID);
-					break;
-				case PickerType.TerrainFeature:
-					__result = new SpriteAddress("TerrainFeatures", skinID);
-					break;
-				case PickerType.Roof:
-					__result = new SpriteAddress("Units", "roof_" + skinID);
-					break;
-				case PickerType.PolytaurHead:
-					__result = new SpriteAddress("Units", "polytaur_2_" + skinID);
-					break;
-				case PickerType.Animal:
-					__result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Game, skinID);
-					break;
-				case PickerType.Fruit:
-					__result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Fruit, skinID);
-					break;
-				case PickerType.Forest:
-					__result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Forest, skinID);
-					break;
-				case PickerType.Mountain:
-					__result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Mountain, skinID);
-					break;
-				case PickerType.Monument_1:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument1, skinID);
-					break;
-				case PickerType.Monument_2:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument2, skinID);
-					break;
-				case PickerType.Monument_3:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument3, skinID);
-					break;
-				case PickerType.Monument_4:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument4, skinID);
-					break;
-				case PickerType.Monument_5:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument5, skinID);
-					break;
-				case PickerType.Monument_6:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument6, skinID);
-					break;
-				case PickerType.Monument_7:
-					__result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument7, skinID);
-					break;
-			}
-			return false;
-		}
+		// [HarmonyPrefix]
+		// [HarmonyPatch(typeof(SpriteData), nameof(SpriteData.GetAddress))]
+		// public static bool SpriteData_GetAddress(ref SpriteAddress __result, PickerType pickerType, string skinID)
+		// {
+			// switch (pickerType)
+			// {
+				// case PickerType.Head:
+					// __result = SpriteData.GetHeadSpriteAddress(skinID);
+					// break;
+				// case PickerType.Unit:
+					// __result = new SpriteAddress("Units", skinID);
+					// break;
+				// case PickerType.TerrainFeature:
+					// __result = new SpriteAddress("TerrainFeatures", skinID);
+					// break;
+				// case PickerType.Roof:
+					// __result = new SpriteAddress("Units", "roof_" + skinID);
+					// break;
+				// case PickerType.PolytaurHead:
+					// __result = new SpriteAddress("Units", "polytaur_2_" + skinID);
+					// break;
+				// case PickerType.Animal:
+					// __result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Game, skinID);
+					// break;
+				// case PickerType.Fruit:
+					// __result = SpriteData.GetResourceSpriteAddress(ResourceData.Type.Fruit, skinID);
+					// break;
+				// case PickerType.Forest:
+					// __result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Forest, skinID);
+					// break;
+				// case PickerType.Mountain:
+					// __result = SpriteData.GetTileSpriteAddress(Polytopia.Data.TerrainData.Type.Mountain, skinID);
+					// break;
+				// case PickerType.Monument_1:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument1, skinID);
+					// break;
+				// case PickerType.Monument_2:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument2, skinID);
+					// break;
+				// case PickerType.Monument_3:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument3, skinID);
+					// break;
+				// case PickerType.Monument_4:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument4, skinID);
+					// break;
+				// case PickerType.Monument_5:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument5, skinID);
+					// break;
+				// case PickerType.Monument_6:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument6, skinID);
+					// break;
+				// case PickerType.Monument_7:
+					// __result = SpriteData.GetBuildingSpriteAddress(ImprovementData.Type.Monument7, skinID);
+					// break;
+			// }
+			// return false;
+		// }
 
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(AudioManager), nameof(AudioManager.SetAmbienceClimate))]
@@ -171,37 +171,37 @@ namespace PolyMod
 			}
 		}
 
-		[HarmonyPrefix]
-		[HarmonyPatch(typeof(SpriteAtlasManager), nameof(SpriteAtlasManager.LoadSpriteAtlasTexture))]
-		private static bool SpriteAtlasManager_LoadSpriteAtlasTexture(SpriteAtlasManager __instance, string atlas, Il2CppSystem.Action<Texture2D> completion)
-		{
-			Dictionary<string, Sprite> sprites = new();
+		// [HarmonyPrefix]
+		// [HarmonyPatch(typeof(SpriteAtlasManager), nameof(SpriteAtlasManager.LoadSpriteAtlasTexture))]
+		// private static bool SpriteAtlasManager_LoadSpriteAtlasTexture(SpriteAtlasManager __instance, string atlas, Il2CppSystem.Action<Texture2D> completion)
+		// {
+			// Dictionary<string, Sprite> sprites = new();
 
-			if (atlas != "Heads") return true;
-			if (!__instance.cachedSprites.TryGetValue(atlas, out _))
-			{
-				__instance.cachedSprites[atlas] = new();
-			}
-			foreach (var sprite in __instance.cachedSprites[atlas])
-			{
-				if (sprite.Value.name != string.Empty) break;
-				sprites.Add(sprite.Key, sprite.Value);
-			}
-			Texture2D customAtlas = new(1, 1);
-			Rect[] rects = customAtlas.PackTextures(sprites.Select(i => i.Value.texture).ToArray(), 2);
-			foreach (var sprite in sprites)
-			{
-				Sprite newSprite = Sprite.Create(
-					customAtlas,
-					rects[sprites.Values.ToList().IndexOf(sprite.Value)],
-					new(0.5f, 0.5f)
-				);
-				__instance.cachedSprites[atlas][sprite.Key] = newSprite;
-				//__instance.spriteToAtlasName[newSprite] = atlas;
-			}
-			completion.Invoke(customAtlas);
-			return false;
-		}
+			// if (atlas != "Heads") return true;
+			// if (!__instance.cachedSprites.TryGetValue(atlas, out _))
+			// {
+				// __instance.cachedSprites[atlas] = new();
+			// }
+			// foreach (var sprite in __instance.cachedSprites[atlas])
+			// {
+				// if (sprite.Value.name != string.Empty) break;
+				// sprites.Add(sprite.Key, sprite.Value);
+			// }
+			// Texture2D customAtlas = new(1, 1);
+			// Rect[] rects = customAtlas.PackTextures(sprites.Select(i => i.Value.texture).ToArray(), 2);
+			// foreach (var sprite in sprites)
+			// {
+				// Sprite newSprite = Sprite.Create(
+					// customAtlas,
+					// rects[sprites.Values.ToList().IndexOf(sprite.Value)],
+					// new(0.5f, 0.5f)
+				// );
+				// __instance.cachedSprites[atlas][sprite.Key] = newSprite;
+				// //__instance.spriteToAtlasName[newSprite] = atlas;
+			// }
+			// completion.Invoke(customAtlas);
+			// return false;
+		// }
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(TechItem), nameof(TechItem.GetUnlockItems))]
@@ -235,33 +235,33 @@ namespace PolyMod
 					{
 						_patches.Add(JObject.Parse(new StreamReader(entry.Open()).ReadToEnd()));
 					}
-					if (Path.GetExtension(name) == ".png")
-					{
-						_sprites.Add(name, entry.ReadBytes());
-					}
+					// if (Path.GetExtension(name) == ".png")
+					// {
+						// _sprites.Add(name, entry.ReadBytes());
+					// }
 				}
 			}
 		}
 
 		public static void Load(JObject gameLogicdata)
 		{
-			GameManager.GetSpriteAtlasManager().cachedSprites.TryAdd("Heads", new());
+			// GameManager.GetSpriteAtlasManager().cachedSprites.TryAdd("Heads", new());
 			foreach (var patch in _patches){
 				try
 				{
 					GameLogicDataPatch(gameLogicdata, patch);
 				} catch {}
 			}
-			foreach (var sprite_ in _sprites){
-				Vector2 pivot = Path.GetFileNameWithoutExtension(sprite_.Key).Split("_")[0] switch
-				{
-					"field" => new(0.5f, 0.0f),
-					"mountain" => new(0.5f, -0.375f),
-					_ => new(0.5f, 0.5f),
-				};
-				Sprite sprite = BuildSprite(sprite_.Value, pivot);
-				GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].Add(Path.GetFileNameWithoutExtension(sprite_.Key), sprite);
-			}
+			// foreach (var sprite_ in _sprites){
+				// Vector2 pivot = Path.GetFileNameWithoutExtension(sprite_.Key).Split("_")[0] switch
+				// {
+					// "field" => new(0.5f, 0.0f),
+					// "mountain" => new(0.5f, -0.375f),
+					// _ => new(0.5f, 0.5f),
+				// };
+				// Sprite sprite = BuildSprite(sprite_.Value, pivot);
+				// GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].Add(Path.GetFileNameWithoutExtension(sprite_.Key), sprite);
+			// }
 		}
 
 		private static void PolyscriptLoad(byte[] polyscriptData)
@@ -339,28 +339,28 @@ namespace PolyMod
 			gld.Merge(patch, Plugin.GLD_MERGE_SETTINGS);
 		}
 
-		private static SpriteAddress GetSprite(SpriteAddress sprite, string name, string style = "", int level = 0)
-		{
-			GetSpriteIfFound($"{name}__", ref sprite);
-			GetSpriteIfFound($"{name}_{style}_", ref sprite);
-			GetSpriteIfFound($"{name}__{level}", ref sprite);
-			GetSpriteIfFound($"{name}_{style}_{level}", ref sprite);
-			return sprite;
-		}
+		// private static SpriteAddress GetSprite(SpriteAddress sprite, string name, string style = "", int level = 0)
+		// {
+			// GetSpriteIfFound($"{name}__", ref sprite);
+			// GetSpriteIfFound($"{name}_{style}_", ref sprite);
+			// GetSpriteIfFound($"{name}__{level}", ref sprite);
+			// GetSpriteIfFound($"{name}_{style}_{level}", ref sprite);
+			// return sprite;
+		// }
 
-		private static void GetSpriteIfFound(string id, ref SpriteAddress sprite)
-		{
-			if (GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].TryGetValue(id, out _))
-			{
-				sprite = new SpriteAddress("Heads", id);
-			}
-		}
+		// private static void GetSpriteIfFound(string id, ref SpriteAddress sprite)
+		// {
+			// if (GameManager.GetSpriteAtlasManager().cachedSprites["Heads"].TryGetValue(id, out _))
+			// {
+				// sprite = new SpriteAddress("Heads", id);
+			// }
+		// }
 
-		private static Sprite BuildSprite(byte[] data, Vector2 pivot)
-		{
-			Texture2D texture = new(1, 1);
-			texture.LoadImage(data);
-			return Sprite.Create(texture, new(0, 0, texture.width, texture.height), pivot, 2112);
-		}
+		// private static Sprite BuildSprite(byte[] data, Vector2 pivot)
+		// {
+			// Texture2D texture = new(1, 1);
+			// texture.LoadImage(data);
+			// return Sprite.Create(texture, new(0, 0, texture.width, texture.height), pivot, 2112);
+		// }
 	}
 }
